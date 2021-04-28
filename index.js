ddf5fc2496d052cc66775718c78b17a2f5ba2202
mesut habibpasa <mesut.habibpasa@gmail.com>
2021-04-28 23:35:41 +0300

    nickname

--- index.js
+++ index.js
@@ -16,16 +16,17 @@ server.listen(3000, () => {
   console.log('listening on *:3000');
 });
 
+const nickNames = {}
 io.on('connection', (socket) => {
-  console.log('a user connected');
   io.emit('chat message', 'a user connected')
   socket.on('disconnect', () => {
-    console.log('user disconnected');
     io.emit('chat message', 'a user disconnected')
   });
   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    socket.broadcast.emit('chat message', msg);
+    socket.broadcast.emit('chat message', `${nickNames[socket.id]}: ${msg}`);
+  });
+  socket.on('set nickname', (nick) => {
+    nickNames[socket.id] = nick;
   });
 });
 
