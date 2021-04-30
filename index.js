82b8a92333ca1125aa5ab4de9303d338e2481e2f
mesuthabibpasa <mesut.habibpasa@gmail.com>
2021-05-01 00:43:50 +0300

    typing status

--- index.js
+++ index.js
@@ -17,17 +17,31 @@ server.listen(3000, () => {
 });
 
 const nickNames = {}
+const whoTypes = {}
 
 io.on('connection', (socket) => {
   io.emit('chat message', 'a user connected')
   socket.on('disconnect', () => {
     io.emit('chat message', 'a user disconnected')
+    delete whoTypes[nickNames[socket.id]];
     delete nickNames[socket.id];
     io.emit('userlist changed', JSON.stringify(nickNames));
+    io.emit('isTyping', JSON.stringify(whoTypes));
   });
+
   socket.on('chat message', (msg) => {
     socket.broadcast.emit('chat message', `${nickNames[socket.id]}: ${msg}`);
   });
+
+  socket.on('typing', (data) => {
+    if(data.typing){
+      whoTypes[nickNames[data.user]] = data.typing;
+    } else {
+      delete whoTypes[nickNames[data.user]];
+    }
+    socket.broadcast.emit('isTyping', JSON.stringify(whoTypes));
+  });
+
   socket.on('set nickname', (nick) => {
     nickNames[socket.id] = nick;
     io.emit('userlist changed', JSON.stringify(nickNames));
